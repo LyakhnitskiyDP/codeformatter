a4db4c09be6f9e46c8faccfc58bfe8ee5bdcd331
lyakhnitskiydp <lyakhnitskiydp@gmail.com>
2021-08-31 21:41:50 +0300

    Add defaults to transitions and commands

--- src/main/java/org/codeformatter/formatters/impl/DefaultFormatterCommandRepository.java
+++ src/main/java/org/codeformatter/formatters/impl/DefaultFormatterCommandRepository.java
@@ -12,70 +12,94 @@ public class DefaultFormatterCommandRepository implements FormatterCommandReposi
 
     private static final Map<Pair<FormatterState, String>, FormatterCommand> commands = new HashMap<>();
 
-    private static final FormatterCommand defaultStringLiteralCommand;
-    private static final FormatterCommand defaultMultilineCommentEndCommand;
+    private static final Map<FormatterState, Map<String, FormatterCommand>> commandMaps;
+    private static final Map<String, FormatterCommand> initialStateCommands;
+    private static final Map<String, FormatterCommand> writingLineStateCommands;
+    private static final Map<String, FormatterCommand> writingStringLiteralStateCommands;
+
+    private static final Map<FormatterState, FormatterCommand> defaultFormatterCommands;
 
     static {
 
-        defaultStringLiteralCommand = (commandToken, context) -> { context.writeToken(commandToken); };
-        defaultMultilineCommentEndCommand = (commandToken, context) -> { context.writeNewLine(); context.writeToken(commandToken);  };
-
-
-        //Commands for INITIAL state
-        commands.putAll(Map.of(
-                Pair.of(FormatterState.of(FormatterState.INITIAL), "Char"), (token, context) -> {
-                    context.writeIndent();
-                    context.writeToken(token);
-                },
-                Pair.of(FormatterState.of(FormatterState.INITIAL), "for"), (token, context) -> {
-                    context.writeIndent();
-                    context.writeToken(token);
-                },
-                Pair.of(FormatterState.of(FormatterState.INITIAL), "Whitespace"), (token, context) -> { },
-                Pair.of(FormatterState.of(FormatterState.MULTILINE_COMMENT_END_2), "Whitespace"), (token, context) -> { },
-                Pair.of(FormatterState.of(FormatterState.INITIAL), "Semicolon"), (token, context) -> {
-                    context.writeToken(token);
-                    context.writeNewLine();
-                },
-                Pair.of(FormatterState.of(FormatterState.INITIAL), "Opening Curly Bracket"), (token, context) -> {
-                    context.writeToken(token);
-                    context.writeNewLine();
-                    context.increaseIndentation();
-                    context.writeIndent();
-                },
-                Pair.of(FormatterState.of(FormatterState.INITIAL), "Closing Curly Bracket"), (token, context) -> {
-                    context.decreaseIndentation();
-                    context.writeIndent();
-                    context.writeToken(token);
-                    context.writeNewLine();
-                }
-            )
+        defaultFormatterCommands = Map.of(
+            FormatterState.of(FormatterState.WRITING_STRING_LITERAL), (commandToken, context) -> { context.writeToken(commandToken); }
         );
-        //Commands for WRITING_LINE STATE
-        commands.putAll(Map.of(
-                Pair.of(FormatterState.of(FormatterState.WRITING_LINE), "Char"), (token, context) -> { context.writeToken(token); },
 
-                Pair.of(FormatterState.of(FormatterState.WRITING_LINE), "Quotes"), (token, context) -> { context.writeToken(token); },
+        initialStateCommands = Map.of(
+                "Char", (token, context) -> {
+                    context.writeIndent();
+                    context.writeToken(token);
+                },
+                 "for", (token, context) -> {
+                    context.writeIndent();
+                    context.writeToken(token);
+                },
+                "Whitespace", (token, context) -> {
 
-                Pair.of(FormatterState.of(FormatterState.WRITING_LINE), "for"), (token, context) -> { context.writeToken(token); },
+                },
+                "Semicolon", (token, context) -> {
+                    context.writeToken(token);
+                    context.writeNewLine();
+                },
+                "Opening Curly Bracket", (token, context) -> {
+                    context.writeToken(token);
+                    context.writeNewLine();
+                    context.increaseIndentation();
+                    context.writeIndent();
+                },
+                "Closing Curly Bracket", (token, context) -> {
+                    context.decreaseIndentation();
+                    context.writeIndent();
+                    context.writeToken(token);
+                    context.writeNewLine();
+                }
+            );
 
-                Pair.of(FormatterState.of(FormatterState.WRITING_LINE), "Whitespace"), (token, context) -> { context.writeToken(token); },
+        writingStringLiteralStateCommands = Map.of();
 
-                Pair.of(FormatterState.of(FormatterState.WRITING_LINE), "Semicolon"), (token, context) -> { context.writeToken(token); context.writeNewLine(); },
+        writingLineStateCommands = Map.of(
+                 "Char", (token, context) -> { context.writeToken(token); },
 
-                Pair.of(FormatterState.of(FormatterState.WRITING_LINE), "Opening Curly Bracket"), (token, context) -> {
+                "Quotes", (token, context) -> { context.writeToken(token); },
+
+                "for", (token, context) -> { context.writeToken(token); },
+
+                "Whitespace", (token, context) -> { context.writeToken(token); },
+
+                "Semicolon", (token, context) -> {
+                     context.writeToken(token);
+                     context.writeNewLine();
+                },
+
+                "Opening Curly Bracket", (token, context) -> {
                     context.writeToken(token);
                     context.writeNewLine();
                     context.increaseIndentation();
                 },
 
-                Pair.of(FormatterState.of(FormatterState.WRITING_LINE), "Closing Curly Bracket"), (token, context) -> {
+                "Closing Curly Bracket", (token, context) -> {
                     context.decreaseIndentation();
                     context.writeIndent();
                     context.writeToken(token);
                 }
-        ));
+        );
 
+        commandMaps = Map.of(
+                FormatterState.of(FormatterState.INITIAL), initialStateCommands,
+                FormatterState.of(FormatterState.WRITING_LINE), writingLineStateCommands,
+                FormatterState.of(FormatterState.WRITING_STRING_LITERAL), writingStringLiteralStateCommands
+        );
+
+    }
+
+    private Map<String, FormatterCommand> getCommandMapForState(FormatterState state) {
+
+        return commandMaps.get(state);
+    }
+
+    private FormatterCommand getDefaultCommandForState(FormatterState state) {
+
+        return defaultFormatterCommands.get(state);
     }
 
     @Override
@@ -83,32 +107,8 @@ public class DefaultFormatterCommandRepository implements FormatterCommandReposi
             FormatterState formatterState,
             Token token) {
 
-        FormatterCommand commandToReturn = commands.get(Pair.of(formatterState, token.getName()));
-
-        if (commandToReturn == null && stateIsStringLiteral(formatterState)) {
-            return defaultStringLiteralCommand;
-        }
-
-        if (commandToReturn == null && stateIsMultilineCommentEnd(formatterState)) {
-            return defaultMultilineCommentEndCommand;
-        }
-
-        if (commandToReturn == null) {
-
-            return (commandToken, context) -> { context.writeIndent(); context.writeToken(commandToken);};
-        }
-
-        return commandToReturn;
-    }
-
-    private boolean stateIsStringLiteral(FormatterState formatterState) {
-
-        return formatterState.getState().equals(FormatterState.WRITING_STRING_LITERAL);
-    }
-
-    private boolean stateIsMultilineCommentEnd(FormatterState formatterState) {
-
-        return formatterState.getState().equals(FormatterState.MULTILINE_COMMENT_END_2);
+        return getCommandMapForState(formatterState)
+                .getOrDefault(token.getName(), getDefaultCommandForState(formatterState));
     }
 
 }
--- src/main/java/org/codeformatter/formatters/impl/DefaultFormatterStateTransitions.java
+++ src/main/java/org/codeformatter/formatters/impl/DefaultFormatterStateTransitions.java
@@ -2,128 +2,75 @@ package org.codeformatter.formatters.impl;
 
 import static org.codeformatter.formatters.impl.FormatterState.*;
 
-import org.codeformatter.collections.Pair;
 import org.codeformatter.formatters.FormatterStateTransitions;
 import org.codeformatter.tokens.Token;
 
-import java.util.HashMap;
 import java.util.Map;
 
 
 public class DefaultFormatterStateTransitions implements FormatterStateTransitions {
 
-    private static final Map<Pair<FormatterState, String>, FormatterState> transitions = new HashMap<>();
+    private static final Map<FormatterState, Map<String, FormatterState>> transitionMaps;
+    private static final Map<FormatterState, FormatterState> defaultStateTransitions;
 
     private static final Map<String, FormatterState> initialStateTransitions;
     private static final Map<String, FormatterState> writingLineStateTransitions;
     private static final Map<String, FormatterState> writingStringLiteralStateTransitions;
 
-    private static final FormatterState defaultStringLiteralState;
-
-    private static final FormatterState defaultMultilineCommentState;
-    private static final FormatterState defaultMultilineCommentStartState;
-    private static final FormatterState defaultMultilineCommentEndState;
-
     static {
-        defaultStringLiteralState = FormatterState.of(FormatterState.WRITING_STRING_LITERAL);
+        defaultStateTransitions = Map.of(
+                FormatterState.of(WRITING_STRING_LITERAL), FormatterState.of(WRITING_STRING_LITERAL),
+                FormatterState.of(INITIAL), FormatterState.of(WRITING_LINE),
+                FormatterState.of(WRITING_LINE), FormatterState.of(WRITING_LINE),
+                FormatterState.of(WRITING_MULTILINE_COMMENT), FormatterState.of(WRITING_MULTILINE_COMMENT)
+        );
 
-        defaultMultilineCommentState = FormatterState.of(FormatterState.WRITING_MULTILINE_COMMENT);
-        defaultMultilineCommentStartState = FormatterState.of(FormatterState.WRITING_LINE);
-        defaultMultilineCommentEndState = FormatterState.of(FormatterState.INITIAL);
+        initialStateTransitions = Map.of(
+                "Char", FormatterState.of(WRITING_LINE),
+                "Whitespace", FormatterState.of(INITIAL),
+                "Semicolon", FormatterState.of(INITIAL),
+                "Opening Curly Bracket", FormatterState.of(INITIAL),
+                "Closing Curly Bracket", FormatterState.of(INITIAL),
+                "Quotes", FormatterState.of(WRITING_STRING_LITERAL),
+                "Slash", FormatterState.of(MULTILINE_COMMENT_START_1)
+        );
 
-        transitions.putAll(Map.of(
-            Pair.of(FormatterState.of(INITIAL), "Char"), FormatterState.of(WRITING_LINE),
-            Pair.of(FormatterState.of(INITIAL), "Whitespace"), FormatterState.of(INITIAL),
-            Pair.of(FormatterState.of(INITIAL), "Semicolon"), FormatterState.of(INITIAL),
-            Pair.of(FormatterState.of(INITIAL), "Opening Curly Bracket"), FormatterState.of(INITIAL),
-            Pair.of(FormatterState.of(INITIAL), "Closing Curly Bracket"), FormatterState.of(INITIAL),
-            Pair.of(FormatterState.of(INITIAL), "Quotes"), FormatterState.of(WRITING_STRING_LITERAL),
-            Pair.of(FormatterState.of(INITIAL), "Slash"), FormatterState.of(MULTILINE_COMMENT_START_1)
-        ));
+        writingLineStateTransitions = Map.of(
+                "Semicolon", FormatterState.of(INITIAL),
+                "Opening Curly Bracket", FormatterState.of(INITIAL),
+                "Closing Curly Bracket", FormatterState.of(INITIAL),
+                "Quotes", FormatterState.of(WRITING_STRING_LITERAL),
+                "Slash", FormatterState.of(MULTILINE_COMMENT_START_1)
+        );
 
-        transitions.putAll(Map.of(
-            Pair.of(FormatterState.of(WRITING_LINE), "Semicolon"), FormatterState.of(INITIAL),
-            Pair.of(FormatterState.of(WRITING_LINE), "Opening Curly Bracket"), FormatterState.of(INITIAL),
-            Pair.of(FormatterState.of(WRITING_LINE), "Closing Curly Bracket"), FormatterState.of(INITIAL),
-            Pair.of(FormatterState.of(WRITING_LINE), "Quotes"), FormatterState.of(WRITING_STRING_LITERAL),
-            Pair.of(FormatterState.of(WRITING_LINE), "Slash"), FormatterState.of(MULTILINE_COMMENT_START_1)
-        ));
+        writingStringLiteralStateTransitions = Map.of(
+                "Quotes", FormatterState.of(WRITING_LINE)
+        );
 
-        transitions.putAll(Map.of(
-                Pair.of(FormatterState.of(MULTILINE_COMMENT_START_1), "Star"), FormatterState.of(WRITING_MULTILINE_COMMENT),
-                Pair.of(FormatterState.of(WRITING_MULTILINE_COMMENT), "Star"), FormatterState.of(MULTILINE_COMMENT_END_1),
-                Pair.of(FormatterState.of(MULTILINE_COMMENT_END_1), "Slash"), FormatterState.of(MULTILINE_COMMENT_END_2),
-                Pair.of(FormatterState.of(MULTILINE_COMMENT_END_2), "Whitespace"), FormatterState.of(INITIAL)
-        ));
+        transitionMaps = Map.of(
+                FormatterState.of(INITIAL), initialStateTransitions,
+                FormatterState.of(WRITING_LINE), writingLineStateTransitions,
+                FormatterState.of(WRITING_STRING_LITERAL), writingStringLiteralStateTransitions
+        );
 
-        transitions.putAll(Map.of(
-                Pair.of(FormatterState.of(WRITING_STRING_LITERAL), "Quotes"), FormatterState.of(WRITING_LINE)
-        ));
+    }
 
+    private Map<String, FormatterState> getTransitionsForState(FormatterState state) {
+
+        return transitionMaps.get(state);
+    }
+
+    private FormatterState getDefaultStateTransitionFor(FormatterState state) {
+
+        return defaultStateTransitions.get(state);
     }
 
     @Override
     public FormatterState nextState(FormatterState state, Token token) {
 
-        FormatterState stateToReturn = transitions.get(Pair.of(state, token.getName()));
+        return getTransitionsForState(state)
+                .getOrDefault(token.getName(), getDefaultStateTransitionFor(state));
 
-        // get transitions for state( state ).getOrDefault( token, defaultStringLiteralState );
-
-        if (stateToReturn == null && stateIsStringLiteral(state)) {
-
-            return defaultStringLiteralState;
-        }
-
-        if (stateToReturn == null && stateIsMultilineComment(state)) {
-
-            return defaultMultilineCommentState;
-        }
-
-        if (stateToReturn == null && stateIsMultilineCommentEnd(state)) {
-
-            return defaultMultilineCommentState;
-        }
-
-        if (stateToReturn == null && stateIsMultilineCommentEnd2(state)) {
-
-            return defaultMultilineCommentEndState;
-        }
-
-        if (stateToReturn == null && stateIsMultilineCommentStart(state)) {
-
-            return defaultMultilineCommentStartState;
-        }
-
-        if (stateToReturn == null) {
-            return FormatterState.of(FormatterState.WRITING_LINE);
-        }
-
-        return stateToReturn;
     }
 
-    private boolean stateIsStringLiteral(FormatterState formatterState) {
-
-        return formatterState.getState().equals(FormatterState.WRITING_STRING_LITERAL);
-    }
-
-    private boolean stateIsMultilineComment(FormatterState formatterState) {
-
-        return formatterState.getState().equals(FormatterState.WRITING_MULTILINE_COMMENT);
-    }
-
-    private boolean stateIsMultilineCommentStart(FormatterState formatterState) {
-
-        return formatterState.getState().equals(FormatterState.MULTILINE_COMMENT_START_1);
-    }
-
-
-    private boolean stateIsMultilineCommentEnd(FormatterState formatterState) {
-
-        return formatterState.getState().equals(FormatterState.MULTILINE_COMMENT_END_1);
-    }
-
-    private boolean stateIsMultilineCommentEnd2(FormatterState formatterState) {
-
-        return formatterState.getState().equals(FormatterState.MULTILINE_COMMENT_END_2);
-    }
 }
